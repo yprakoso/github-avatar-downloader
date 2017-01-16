38888ceadae9ed7d8ff656f04bbc5a0ffc0e7f6a
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-01-16 22:48:57 +0000

    add requestURL (step 4)

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "yprakoso";
+var GITHUB_TOKEN = "1967e43320f5511a775ad855bdd1fedf114436fb";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
