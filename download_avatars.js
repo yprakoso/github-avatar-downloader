0f83135eb46f3f3cc9a570ff1f7a797760889fca
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-01-17 01:40:12 +0000

    Make user required arguments (step 10)

--- download_avatars.js
+++ download_avatars.js
@@ -2,32 +2,36 @@ var request = require('request');
 var fs = require('fs');
 
 var myArgs = process.argv.slice(2);
-console.log('Welcome to the GitHub Avatar Downloader!');
+if (myArgs.length === 2) {
+  console.log('Welcome to the GitHub Avatar Downloader!');
 
-var GITHUB_USER = "yprakoso";
-var GITHUB_TOKEN = "1967e43320f5511a775ad855bdd1fedf114436fb";
+  var GITHUB_USER = "yprakoso";
+  var GITHUB_TOKEN = "1967e43320f5511a775ad855bdd1fedf114436fb";
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = {
-    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var requestURL = {
+      url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-Agent': 'GitHub Avatar Downloader - Student Project'
+      }
     }
+
+    request(requestURL, cb);
   }
 
-  request(requestURL, cb);
-}
-
-getRepoContributors(myArgs[0], myArgs[1], function(err, result, body) {
-  var data = JSON.parse(body);
-  var avatars = []
-  data.forEach(function(element){
-    // console.log(element);
-    downloadImageByURL(element.avatar_url, element.login);
+  getRepoContributors(myArgs[0], myArgs[1], function(err, result, body) {
+    var data = JSON.parse(body);
+    var avatars = []
+    data.forEach(function(element){
+      // console.log(element);
+      downloadImageByURL(element.avatar_url, element.login);
+    });
+    // console.log(avatars);
   });
-  // console.log(avatars);
-});
 
-function downloadImageByURL(url, filePath) {
-  request.get(url).pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
-}
+  function downloadImageByURL(url, filePath) {
+    request.get(url).pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
+  }
+} else console.log("You should give 2 arguments, which is <repoOwner> and <repoName>.");
+
+
