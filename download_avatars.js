b4872fb5b1b6b9a63a5556087efed8fd185a19da
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-01-17 00:21:45 +0000

    add make request (step 5) and print each avatar_url (step 6)

--- download_avatars.js
+++ download_avatars.js
@@ -6,11 +6,21 @@ var GITHUB_USER = "yprakoso";
 var GITHUB_TOKEN = "1967e43320f5511a775ad855bdd1fedf114436fb";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var requestURL = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  }
+
+  request(requestURL, cb);
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors("jquery", "jquery", function(err, result, body) {
+  var data = JSON.parse(body);
+  var avatars = []
+  data.forEach(function(element){
+    avatars.push(element.avatar_url);
+  });
+  console.log(avatars);
 });
